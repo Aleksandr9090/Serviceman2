0b0f448525d42a29c65e2607af04aa7c88aabbd4
shutkin.90 <shutkin.90@mail.ru>
2022-11-12 12:42:51 +0400

    Initial Commit

---
+++ Serviceman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Serviceman
+//
+//  Created by Aleksandr on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

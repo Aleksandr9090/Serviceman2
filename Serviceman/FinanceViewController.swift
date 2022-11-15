4ed764f96763252f3953e20b3de25922c9200c95
shutkin.90 <shutkin.90@mail.ru>
2022-11-15 20:41:20 +0400

    work with pop up button

--- Serviceman/FinanceViewController.swift
+++ Serviceman/FinanceViewController.swift
@@ -18,13 +18,64 @@ class FinanceViewController: UIViewController {
         segmentedControl.addTarget(self, action: #selector(segmentedControlChanged), for: .valueChanged)
         return segmentedControl
     }()
+    
+    private lazy var monthPopUpButton: UIButton = {
+        var attributes = AttributeContainer()
+        attributes.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.attributedTitle = AttributedString("December", attributes: attributes)
+        buttonConfiguration.baseForegroundColor = .black
+        buttonConfiguration.baseBackgroundColor = UIColor(
+            red: 238/255,
+            green: 238/255,
+            blue: 239/255,
+            alpha: 1
+        )
+        return UIButton(configuration: buttonConfiguration)
+    }()
+    
+    private lazy var profitLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Profit: "
+        return label
+    }()
+    
+    private lazy var numberOfOrdersLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Number of Orders: "
+        return label
+    }()
+    
+    private lazy var averageCost: UILabel = {
+        let label = UILabel()
+        label.text = "Average Cost: "
+        return label
+    }()
+    
+    private lazy var numberOfWorkingDays: UILabel = {
+        let label = UILabel()
+        label.text = "Number Of Working Days: "
+        return label
+    }()
+    
+    private lazy var numberOfFines: UILabel = {
+        let label = UILabel()
+        label.text = "Number Of Fines: "
+        return label
+    }()
 
-
+    private lazy var amountOfFines: UILabel = {
+        let label = UILabel()
+        label.text = "Amount Of Fines"
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
         setupNavigationBar()
+        setMonthPopUpButton()
     }
     
     private func layout() {
@@ -35,6 +86,88 @@ class FinanceViewController: UIViewController {
             make.trailing.equalToSuperview().offset(-16)
             make.height.equalTo(30)
         }
+        
+        view.addSubview(monthPopUpButton)
+        monthPopUpButton.snp.makeConstraints { make in
+            make.top.equalTo(timePeriodSegmentedControl.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(40)
+        }
+        
+        view.addSubview(profitLabel)
+        profitLabel.snp.makeConstraints { make in
+            make.top.equalTo(monthPopUpButton.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(30)
+        }
+        
+        view.addSubview(numberOfOrdersLabel)
+        numberOfOrdersLabel.snp.makeConstraints { make in
+            make.top.equalTo(profitLabel.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(30)
+        }
+        
+        view.addSubview(averageCost)
+        averageCost.snp.makeConstraints { make in
+            make.top.equalTo(numberOfOrdersLabel.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(30)
+        }
+        
+        view.addSubview(numberOfWorkingDays)
+        numberOfWorkingDays.snp.makeConstraints { make in
+            make.top.equalTo(averageCost.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(30)
+        }
+        
+        view.addSubview(numberOfFines)
+        numberOfFines.snp.makeConstraints { make in
+            make.top.equalTo(numberOfWorkingDays.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(30)
+        }
+        
+        view.addSubview(amountOfFines)
+        amountOfFines.snp.makeConstraints { make in
+            make.top.equalTo(numberOfFines.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(30)
+        }
+        
+        
+    }
+    
+    private func setMonthPopUpButton() {
+//        let optionClosure = {(action: UIAction) in
+//            monthPopUpButton.title = title)
+//        }
+        
+        monthPopUpButton.menu = UIMenu(children: [
+            UIAction(title: "January",state: .on, handler: { action in }),
+            UIAction(title: "February", handler: { action in }),
+            UIAction(title: "March", handler: { action in }),
+            UIAction(title: "April", handler: { action in }),
+            UIAction(title: "May", handler: { action in }),
+            UIAction(title: "June", handler: { action in }),
+            UIAction(title: "July", handler: { action in }),
+            UIAction(title: "August", handler: { action in }),
+            UIAction(title: "September", handler: { action in }),
+            UIAction(title: "October", handler: { action in }),
+            UIAction(title: "November", handler: { action in }),
+            UIAction(title: "December", handler: { action in }),
+        ])
+        
+        monthPopUpButton.showsMenuAsPrimaryAction = true
+        monthPopUpButton.changesSelectionAsPrimaryAction = true
     }
     
     private func setupNavigationBar() {
@@ -72,23 +205,4 @@ class FinanceViewController: UIViewController {
             break
         }
     }
-    
-    
-    
-    
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
 }
